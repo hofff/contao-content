211cde91c1058a0e28d8332e59dc91b3e5b474f4
David Molineus <david.molineus@netzmacht.de>
2024-09-25 08:51:47 +0200

    Remove not required js

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,7 +4,5 @@ declare(strict_types=1);
 
 $GLOBALS['BE_MOD']['content']['article']['stylesheet'][] = 'bundles/hofffcontaocontent/css/hofff_content.css';
 $GLOBALS['BE_MOD']['design']['themes']['stylesheet'][]   = 'bundles/hofffcontaocontent/css/hofff_content.css';
-$GLOBALS['BE_MOD']['content']['article']['javascript'][] = 'bundles/hofffcontaocontent/js/hofff_content.js';
-$GLOBALS['BE_MOD']['design']['themes']['javascript'][]   = 'bundles/hofffcontaocontent/js/hofff_content.js';
 
 $GLOBALS['BE_MOD']['design']['themes']['tables'][] = 'tl_hofff_content';
